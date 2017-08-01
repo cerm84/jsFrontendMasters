6f1f871fdc47712a1670ecb1fdf8bb418c1789b4
Carlos Romero Mejía <cerm84@gmail.com>
2017-08-01 10:25:33 -0600

    Add comments to post exercise

--- intro-to-webdev-app/intro-to-webdev-app/node-exercises/posting/app.js
+++ intro-to-webdev-app/intro-to-webdev-app/node-exercises/posting/app.js
@@ -5,10 +5,11 @@ app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/login', function(req, res){
-  console.log(req.body.username + " " +  req.body.password);
+app.post('/login', function(req, res){//app gets a post function instead of a get one
+  //because of the username and password below, this are parameters to the post
+  console.log(req.body.username + " " +  req.body.password);//prints the name and password in the server log
   if (req.body.username === "brian" && req.body.password === "pass") {
-    res.json(200, {status:"success"});
+    res.json(200, {status:"success"});//we return JSON instead of simple text
   }
   else {
     res.json(401, {status:"failure"})
