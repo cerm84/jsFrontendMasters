ab4874b39fa6599a6391f31973bb158a0e64a2d0
Carlos Romero Mejía <cerm84@gmail.com>
2017-07-31 18:17:10 -0600

    basic app explanation

--- intro-to-webdev-app/intro-to-webdev-app/node-exercises/basic/app.js
+++ intro-to-webdev-app/intro-to-webdev-app/node-exercises/basic/app.js
@@ -1,6 +1,10 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+//calls http library from node
+http.createServer(function (req, res) { //calls the create server function
+  res.writeHead(200, {'Content-Type': 'text/plain'});//re- routes any request in 
+  //the browser to the server app page
+  res.end('Hello World\n');//end the response giving the Hello world line
 }).listen(8080, '127.0.0.1');
+//listen for port 8080 in the local host (127.0.0.1)
 console.log('Server running at http://127.0.0.1:8080/');
+//console.log shows the text in the cmd when the app is run
