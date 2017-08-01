50a0188076e1d0352d9f06686f8120db251dd68c
Carlos Romero Mejía <cerm84@gmail.com>
2017-07-31 22:54:33 -0600

    explnation of parameters in the express app

--- intro-to-webdev-app/intro-to-webdev-app/node-exercises/params/app.js
+++ intro-to-webdev-app/intro-to-webdev-app/node-exercises/params/app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get('/team/:name', function(req, res){
+app.get('/team/:name', function(req, res){//:name ask for a parameter the user inputs in the nav bar
   res.setHeader('Content-Type','text/plain');
   res.send("You picked " + req.params.name);
 });
